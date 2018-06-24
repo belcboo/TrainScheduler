21f2a6b5b61f02f4f1b3115b42ed122613e23c52
Francisco Garcia <belcboo@gmail.com>
2018-06-24 18:38:40 -0400

    Improvements: Refresh every minute the "expected arrival" time.

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -11,8 +11,20 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// Event triggered on click.
 
+//Executes the updater function every minute to update automatically the Expected time on the table
+setInterval(function() {
+  console.log("interval run");
+
+  //Clears all the data in the table to avoid duplicates.
+  $("#trainTable > tbody").empty();
+
+  //Calls updater to create all the rows in the table.
+  updater();
+
+}, 60 * 1000);
+
+// Event triggered on click.
 $("#addTrain").on('click', function(event) {
 
   event.preventDefault();
@@ -24,7 +36,6 @@ $("#addTrain").on('click', function(event) {
   var trainFreq = $("#trainFreq").val().trim();
 
   // Push to db
-
   database.ref().push({
     tName: trainName,
     tOrig: trainOrig,
@@ -33,34 +44,45 @@ $("#addTrain").on('click', function(event) {
   })
 
   // Clean all the text-boxes
-
   $("#trainName").val("");
   $("#trainOrig").val("");
   $("#trainTime").val("");
   $("#trainFreq").val("");
 
+  //Clears the content of the table to avoid repetead items.
+  $("#trainTable > tbody").empty();
+
+  //Calls the function updater to pull all the values including the last one pushed.
+  updater();
+
 
 });
 
 // Pull data from firebase and feed the rows in the table.
+function updater() {
 
-database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
+  //Creates a snapshot from the content in the DB and store it into a temp variable.
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
 
-  //Storing db varuables into variables
+    //Storing db from temp variable into variables
+    var trainName = childSnapshot.val().tName;
+    var trainOrig = childSnapshot.val().tOrig;
+    var trainTime = childSnapshot.val().tTime;
+    var trainFreq = childSnapshot.val().tFreq;
 
-  var trainName = childSnapshot.val().tName;
-  var trainOrig = childSnapshot.val().tOrig;
-  var trainTime = childSnapshot.val().tTime;
-  var trainFreq = childSnapshot.val().tFreq;
+    //Converting the time to display in the table from unix time to HH:MM
+    var fixedTime = moment.unix(trainTime).format("hh:mm A");
+    console.log(fixedTime);
 
-  var fixedTime = moment.unix(trainTime).format("hh:mm A");
-  console.log(fixedTime);
+    //Calculate the difference between the arrival time and the actual time.
+    var mins = moment().diff(moment(trainTime, "X"), "minutes") * (-1);
+    fixedTime
+    console.log(mins);
 
-  var mins = moment().diff(moment(trainTime, "X"), "minutes")*(-1);fixedTime
-  console.log(mins);
+    //Push all the rows of data into the table.
+    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainOrig + "</td><td>" + trainFreq + "</td><td>" + fixedTime + "</td><td>" + mins + "</td></tr>");
+  });
+}
 
-  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainOrig+ "</td><td>" + trainFreq + "</td><td>" + fixedTime + "</td><td>" + mins + "</td></tr>");
-
-
-});
+updater();
