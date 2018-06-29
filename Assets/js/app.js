dafd56bdb160df023b3c2a1c58b2030ec3fc112a
Francisco Garcia <belcboo@gmail.com>
2018-06-28 22:25:14 -0400

    Fixed bugs. Next train function working.

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 var config = {
-  apiKey: "",
+  apiKey: "AIzaSyA16N0RP_nel7lvaTAgjKXpvPc8yjGjgoM",
   authDomain: "vast-math-155602.firebaseapp.com",
   databaseURL: "https://vast-math-155602.firebaseio.com",
   projectId: "vast-math-155602",
@@ -33,13 +33,16 @@ $("#addTrain").on('click', function(event) {
   // Getting values of the inputs
   var trainName = $("#trainName").val().trim();
   var trainOrig = $("#trainOrig").val().trim();
-  var trainTime = moment($("#trainTime").val().trim(), "HH:mm").format("X");
-  var trainFreq = moment($("#trainFreq").val().trim(), 'HH:mm').diff(moment().startOf('day'), 'seconds');
+  // var trainTime = moment($("#trainTime").val().trim(), "HH:mm").format("X");
+  // var trainFreq = moment($("#trainFreq").val().trim(), 'HH:mm').diff(moment().startOf('day'), 'seconds');
+  var trainTime = $("#trainTime").val().trim();
+  var trainFreq = $("#trainFreq").val().trim();
+
 
   console.log(trainFreq);
 
   // Push to db
-  database.ref().push({
+  database.ref("trains").push({
     tName: trainName,
     tOrig: trainOrig,
     tTime: trainTime,
@@ -65,46 +68,70 @@ $("#addTrain").on('click', function(event) {
 function updater() {
 
   //Creates a snapshot from the content in the DB and store it into a temp variable.
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+  database.ref("trains").on("child_added", function(childSnapshot) {
+      console.log(childSnapshot.val());
 
-    if(childSnapshot.val().tTime >= 0){
 
-      var oldArrival = parseInt(childSnapshot.val().tTime);
-      var trainFreq = parseInt(childSnapshot.val().tFreq);
-      var newArrival = oldArrival + trainFreq;
+      //Storing db from temp variable into variables
+      var trainName = childSnapshot.val().tName;
+      var trainOrig = childSnapshot.val().tOrig;
+      var trainTime = childSnapshot.val().tTime;
+      var trainFreq = childSnapshot.val().tFreq;
 
-      // database.ref().set({
-      //   tTime: newArrival
-      // });
-      console.log("new push");
-      console.log("oldArrival " + oldArrival);
-      console.log("newArrival " + newArrival);
+      //Converting the time to display in the table from unix time to HH:MM
+      // var fixedTime = moment.unix(trainTime).format("hh:mm A");
 
-      //This if was supposed to update the new time of arrival for the train but
-      //I couldnt figure how to set the new value in the same kay lol.
+      var tTimeconverted = moment(trainTime, "HH:mm").subtract(1, "years");
+      console.log(tTimeconverted);
+      //Convert seconds to HH;mm
+      // var fixedFreq = moment.utc(trainFreq * 1000).format('HH:mm');
 
-    };
+      var currentTime = moment();
+      console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+      //Calculate the difference between the arrival time and the actual time.
+      // var mins = moment().diff(moment(trainTime, "X"), "minutes") * (-1);
+      // fixedTime
 
-    //Storing db from temp variable into variables
-    var trainName = childSnapshot.val().tName;
-    var trainOrig = childSnapshot.val().tOrig;
-    var trainTime = childSnapshot.val().tTime;
-    var trainFreq = childSnapshot.val().tFreq;
+      var diffTime = moment().diff(moment(tTimeconverted), "minutes");
+      console.log(diffTime);
 
-    //Converting the time to display in the table from unix time to HH:MM
-    var fixedTime = moment.unix(trainTime).format("hh:mm A");
+      var tRemainder = diffTime % trainFreq;
+      console.log(tRemainder);
 
-    //Convert seconds to HH;mm
-    var fixedFreq = moment.utc(trainFreq*1000).format('HH:mm');
+      var minutesTillTrain = trainFreq - tRemainder;
+      console.log(minutesTillTrain);
 
-    //Calculate the difference between the arrival time and the actual time.
-    var mins = moment().diff(moment(trainTime, "X"), "minutes") * (-1);fixedTime
+      var nextTrain = moment().add(minutesTillTrain, "minutes");
+      console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+      var nextTrain_fixed = moment(nextTrain).format("hh:mm A");
+
     // console.log(mins);
+
+    // if (mins < 0) {
+    //
+    //   var oldArrival = parseInt(childSnapshot.val().tTime);
+    //   var trainFreq = parseInt(childSnapshot.val().tFreq);
+    //   var newArrival = oldArrival + trainFreq;
+    //
+    //
+    //   console.log("new push");
+    //   console.log("oldArrival " + oldArrival);
+    //   console.log("newArrival " + newArrival);
+    //
+    //   var hopperRef = usersRef.child("gracehop");
+    //   hopperRef.update({
+    //     "nickname": "Amazing Grace"
+    //   });
+    //
+    //   var query = database.ref("trains").orderByChild("tTime").equalTo(oldArrival);
+    //   query.update({tTime:newArrival});
+    //   console.log(query);
+    //
+    // };
 
 
     //Push all the rows of data into the table.
-    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainOrig + "</td><td>" + fixedFreq + "</td><td>" + fixedTime + "</td><td>" + mins + "</td></tr>");
+    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainOrig + "</td><td>" + trainFreq + "</td><td>" + nextTrain_fixed + "</td><td>" + minutesTillTrain + "</td></tr>");
   });
 }
 
